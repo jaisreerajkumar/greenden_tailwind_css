ee823186fb88e39855d644ccc4413adffc1d6089
jayasreerajkumar <jaisreerajkumar@gmail.com>
2025-10-23 11:58:53 +0530

    side navbar has been amended

---
+++ product.js
@@ -0,0 +1,30 @@
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+menuicon.addEventListener("click",function()
+{
+    sidenav.style.right=0;
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+var productsearch=document.getElementById("productsearch")
+var search=document.getElementById("search")
+var productlist=productsearch.querySelectorAll("div")
+search.addEventListener("keyup",function(){
+    var enteredValue=event.target.value.toUppercase()
+})
+for(count=0;count<productlist.length;count=count+1)
+{
+    var productname=productlist[count].querySelectorAll("h1").textContent
+    if(productname.toUpperCase().indexOf(enteredValue)<0)
+    {
+        productlist[count].style.display="none"
+    }
+    else{
+        productlist[count].style.display="block"
+    }
+
+}
